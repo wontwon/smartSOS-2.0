2177e63a8d707c5513b07843ce5245d316170943
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-06-17 18:46:33 -0400

    Add factories for donor

--- spec/factories/donors.rb
+++ spec/factories/donors.rb
@@ -1,6 +1,16 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
+require 'faker'
 
-FactoryGirl.define do
-  factory :donor do
-  end
+FactoryGirl.define do 
+	factory :donor do 
+		first_name { Faker::Name.first_name }
+		last_name { Faker::Name.last_name }
+		email { Faker::Internet.email }
+		password { "password" }
+		password_confirmation { "password" }
+
+		factory :invalid_donor do 
+			first_name { nil }
+		end
+
+	end
 end
