40a4baa7321854a295c88a8acb34ad8aa02c9824
Pei Xiong Liu <peixiong.liu@gmail.com>
2014-06-17 15:57:17 -0400

    Initial commit, SmartSOS 2.0

---
+++ smartsos/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smartsos::Application.config.secret_key_base = 'ec54bf09adcbbdf3252d29a68d949fef182ca417181da51fa3f69b2e56d6dc281d055c54c7f00cf577ff82313550d485f5377369dccbf3a2515fed9995bf2e71'
