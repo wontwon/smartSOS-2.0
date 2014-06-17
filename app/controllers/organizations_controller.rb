e72bcd8567f7bb9d473f1688b46cb8f8c9e3af81
Donger" & Alexander Frankel <pair+wontwon.xander-f@devbootcamp.com>
2014-06-17 19:03:51 -0400

    Change file names to reflect organizations view directory

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,4 +1,4 @@
-class OrganizationController < ApplicationController
+class OrganizationsController < ApplicationController
   before_action :set_organization, only: [:show, :edit, :update]
 
   def show
