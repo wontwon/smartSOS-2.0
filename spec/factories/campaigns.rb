6cce18ed0465a4cf336c1596a91950cb9174e644
Pei Liu <peixiong.liu@gmail.com>
2014-06-17 20:58:11 -0400

    Fill in factories for campaigns, items, organizations, pledges, and requests

--- spec/factories/campaigns.rb
+++ spec/factories/campaigns.rb
@@ -2,5 +2,13 @@
 
 FactoryGirl.define do
   factory :campaign do
+    association :organization
+    name { Faker::Lorem.word }
+    description { Faker::Lorem.paragraph }
+    end_date { '02-10-2014' }
+
+    factory :invalid_campaign do
+      name nil
+    end
   end
 end
--- spec/factories/requests.rb
+++ spec/factories/requests.rb
@@ -2,5 +2,8 @@
 
 FactoryGirl.define do
   factory :request do
+    association :campaign
+    association :item
+    quantity: { rand(10) }
   end
 end
