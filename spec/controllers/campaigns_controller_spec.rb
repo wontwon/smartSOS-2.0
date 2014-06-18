d966ffad32327814e47da574e6d565d802a9e534
Pei Liu <peixiong.liu@gmail.com>
2014-06-17 20:18:45 -0400

    Create campaigns_controller_spec file.

---
+++ spec/controllers/campaigns_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe CampaignsController do
+
+  describe 'GET #index' do
+  end
+
+  describe 'GET #new' do
+  end
+
+  describe 'POST #create' do
+  end
+
+  describe 'GET #edit' do
+  end
+
+  describe 'GET #show' do
+  end
+
+  describe 'POST #update' do
+  end
+
+end
