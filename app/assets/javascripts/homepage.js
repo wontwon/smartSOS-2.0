e5df7eb9d0c1c7ed7ae13f675e42530068f99ab7
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-06-18 14:33:11 -0400

    Finish homepage

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,6 +1,32 @@
 function animateHomepage() {
-	// $('#nav').css({opacity: 0.0, visibility: "visible"}).delay(5000).css({opacity: 1.0, visibility: "visible"});
-	$('#quote').hide().fadeIn(1000);
+	// $('#nav').hide().delay(1000).fadeIn(500);
+	// $('#nav').css({opacity: 0.0, visibility: "visible"}).delay(1000).css({opacity: 1.0, visibility: "visible"});
+	$('#quote').hide().fadeIn(3000);
+}
+
+	var scrollY = 0;
+	var distance = 40;
+	var speed = 24;
+
+function autoScrollTo(el) {
+		var currentY = window.pageYOffset;
+		// var targetY = document.getElementById(el).offsetTop;
+		var targetY = $(el).offsetTop;
+		var bodyHeight = document.body.offsetHeight;
+		var yPos = currentY + window.innerHeight;
+		var animator = setTimeout('autoScrollTo(\' '+el+' \')', speed);
+
+		if (yPos > bodyHeight){
+			clearTimeout(animator);
+		} else {
+			if (currentY < targetY - distance) {
+				scrollY = currentY + distance;
+				window.scroll(0, scrollY);
+			} else {
+				clearTimeout(animator);
+			}
+		}
+			
 }
 
 
