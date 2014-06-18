da44839aeb03ad2066d50ad467aff6fc46aac503
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-06-18 11:49:46 -0400

    Add design 50% done

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,4 +1,6 @@
 function animateHomepage() {
-	// $('#nav').hide().fadeIn(1500);
-	// $('#quote').hide().fadeIn(1000);
+	// $('#nav').css({opacity: 0.0, visibility: "visible"}).delay(5000).css({opacity: 1.0, visibility: "visible"});
+	$('#quote').hide().fadeIn(1000);
 }
+
+
