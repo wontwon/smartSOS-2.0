56e99654dea2e794a087588efbd0c439496bc77c
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-06-18 10:06:48 -0400

    Add design foundation

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,9 +1,4 @@
 function animateHomepage() {
-	$('#logo').hide().fadeIn(700);
-	$('#S').hide().fadeIn(1000);
-	$('#O').hide().fadeIn(1300);
-	$('#S_2').hide().fadeIn(1600);
-	$('#button').hide().fadeIn(700);
-	$('#quote').hide().fadeIn(1000);
-	$('#nav').hide().fadeIn(500);
+	// $('#nav').hide().fadeIn(1500);
+	// $('#quote').hide().fadeIn(1000);
 }
