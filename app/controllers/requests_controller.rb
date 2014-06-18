5d715a13c212cba4931e652e2a57f8b50d731bae
Donger <tuanduong15@gmail.com>
2014-06-17 20:45:47 -0400

    Add new and create action

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,8 +1,19 @@
 class RequestsController < ApplicationController
   def new
+    @request = Request.new
+    @items = Item.all
   end
 
   def create
+    @request = Request.new(request_params)
+
+    if @request.save
+      flash[:notice] = "Item added"
+      redirect_to  new_campaign_request_path(params[:campaign_id])
+    else
+      flash[:alert] = "#{@request.errors.full_messages}"
+      redirect_to  make_requests_path(@campaign.organization, @campaign)
+    end
   end
 
   def edit
@@ -10,5 +21,12 @@ class RequestsController < ApplicationController
 
   def update
   end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:campaign_id, :item_id, :quantity)
+  end
+
 
 end
