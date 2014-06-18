84484fa3a14a160d2cf67e774cd511da15f3b5c6
Donger <tuanduong15@gmail.com>
2014-06-17 22:15:58 -0400

    Port Bootstrap CSS into application from SmartSOS 1.0

---
+++ app/assets/javascripts/homepage.js
@@ -0,0 +1,9 @@
+function animateHomepage() {
+	$('#logo').hide().fadeIn(700);
+	$('#S').hide().fadeIn(1000);
+	$('#O').hide().fadeIn(1300);
+	$('#S_2').hide().fadeIn(1600);
+	$('#button').hide().fadeIn(700);
+	$('#quote').hide().fadeIn(1000);
+	$('#nav').hide().fadeIn(500);
+}
---
+++ app/assets/javascripts/prepare.js
@@ -0,0 +1,4 @@
+function prepareApp(){
+	animateHomepage();
+	amazonNewTab();
+}
