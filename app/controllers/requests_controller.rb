3c5f13d2bbade350c4a48da42efa492d3c1be26f
Donger <tuanduong15@gmail.com>
2014-06-17 21:05:33 -0400

    Port controller actions from SmartSOS 1.0

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,8 @@
 class RequestsController < ApplicationController
+before_action :set_campaign, only: [:edit, :new]
+
   def new
+    @organization = @campaign.organization
     @request = Request.new
     @items = Item.all
   end
@@ -17,15 +20,28 @@ class RequestsController < ApplicationController
   end
 
   def edit
+    @requests = @campaign.requests
   end
 
   def update
+    if @request.update(request_params)
+      render :edit
+    else
+      flash[:alert] = @request.errors.fulL_messages
+      render :edit
+    end
   end
 
   private
 
   def request_params
     params.require(:request).permit(:campaign_id, :item_id, :quantity)
+  end
+
+  def set_campaign
+    @campaign = Campaign.find(params[:campaign_id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The campaign you were looking for could not be found."
   end
 
 
